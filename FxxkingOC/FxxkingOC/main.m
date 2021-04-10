4c6de23ee6c6ec927a74849196b44415b4dbcea7
Rick <382542165@qq.com>
2021-04-10 16:51:31 +0800

    Initial

---
+++ FxxkingOC/FxxkingOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FxxkingOC
+//
+//  Created by Rick on 2021/4/10.
+//  Copyright © 2021 Rick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
